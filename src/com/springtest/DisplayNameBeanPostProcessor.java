1a0d7788e0bac115d0bb639870efa94bed4db028
mohan22 <jakkula4@gmail.com>
2019-06-25 23:00:50 +0530

    Bean post processor before and after bean creation

---
+++ src/com/springtest/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,22 @@
+package com.springtest;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("postProcessAfterInitialization " + beanName);
+		return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		System.out.println("postProcessBeforeInitialization " + beanName);
+		return BeanPostProcessor.super.postProcessBeforeInitialization(bean, beanName);
+	}
+
+}
