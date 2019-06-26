6a655c387f54d93ea34fd5583a5c49236414b131
mohan22 <jakkula4@gmail.com>
2019-06-26 12:08:28 +0530

    autowire using qualifiers

--- src/com/springtest/Circle.java
+++ src/com/springtest/Circle.java
@@ -1,6 +1,7 @@
 package com.springtest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
 
@@ -11,6 +12,7 @@ public class Circle implements Shape {
 	}
 
 	@Autowired
+	@Qualifier("circleCenter")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
