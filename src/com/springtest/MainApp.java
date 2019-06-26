4a45f3d323eed7193d0ee9cc70dab638a125a145
mohan22 <jakkula4@gmail.com>
2019-06-26 10:39:33 +0530

    BeanFactoryPostProcessor

--- src/com/springtest/MainApp.java
+++ src/com/springtest/MainApp.java
@@ -1,16 +1,21 @@
 package com.springtest;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
+	static Log log = LogFactory.getLog(MainApp.class.getName());
+
 	public static void main(String[] args) {
-		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		context.registerShutdownHook();
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 		// obj.getMessage();
 		Triangle tri = (Triangle) context.getBean("triangle");
 		tri.draw();
+
+		log.info("Exiting the program");
 
 	}
 
